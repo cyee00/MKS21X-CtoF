6196ca475719d88fe21b7e1b4bd676ecfdf575d9
cyee00 <johndoe@example.com>
2018-09-18 23:52:53 -0400

    fixing the main function

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,12 @@
-public static double celsiusToFahrenheit(double celsius) {
-	return celsius * (9 .0/5) + 32;
+public class CtoFTester{
+	public static double celsiusToFahrenheit(double celsius) {
+		return celsius * (9.0/5) + 32;
+	}
+	public static double fahrenheitToCelsius(double fahrenheit) {
+		return ((fahrenheit - 32) * 5.0 / 9);
+	}
+	public static double main() {
+		return celsiusToFahrenheit(0);
+		return fahrenheitToCelsius(100);
+	}
 }
