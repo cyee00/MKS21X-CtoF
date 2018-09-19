7c8b3c77170bd6e5ce3590d4c79b8660351c39c2
cyee00 <johndoe@example.com>
2018-09-19 00:01:41 -0400

    need to tweak the math

--- CtoFTester.java
+++ CtoFTester.java
@@ -5,8 +5,7 @@ public class CtoFTester{
 	public static double fahrenheitToCelsius(double fahrenheit) {
 		return ((fahrenheit - 32) * 5.0 / 9);
 	}
-	public static double main() {
-		return celsiusToFahrenheit(0);
-		return fahrenheitToCelsius(100);
+	public static void main(String[] args) {
+		System.out.println(celsiusToFahrenheit(0) + fahrenheitToCelsius(100));
 	}
 }
