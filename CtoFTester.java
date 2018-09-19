bc7a4ae1a9ddad57bd137c53f1b6ba00418fdb9c
cyee00 <johndoe@example.com>
2018-09-19 00:09:08 -0400

    finishedgit add CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -3,9 +3,10 @@ public class CtoFTester{
 		return celsius * (9.0/5) + 32;
 	}
 	public static double fahrenheitToCelsius(double fahrenheit) {
-		return ((fahrenheit - 32) * 5.0 / 9);
+		return ((fahrenheit - 32) * (5.0/9));
 	}
 	public static void main(String[] args) {
-		System.out.println(celsiusToFahrenheit(0) + fahrenheitToCelsius(100));
+		System.out.println(celsiusToFahrenheit(0));
+		System.out.println(fahrenheitToCelsius(212));
 	}
 }
